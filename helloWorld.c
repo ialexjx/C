68e9672b8e0b74d62dafebf2270af84130aac3c9
ialexjx <akshathrx6393@gmail.com>
2023-11-25 21:52:25 +0530

    Initial Commit

---
+++ helloWorld.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+int main(){  
+  printf("This is the first code in C prograamming");
+  return 0;
+}
---
+++ printNumber.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+ int main(){
+    int number = 20;
+    printf("Number is %i", number);
+    return 0;
+ }
