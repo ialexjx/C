671eccbfee1708d54e066c144522f80e019d6080
ialexjx <akshathrx6393@gmail.com>
2023-11-25 23:41:04 +0530

    git ignore file

--- printCharacter.c
+++ printCharacter.c
@@ -3,6 +3,12 @@
 int main(){
     char firstCharacter = 'A';
     char secondCharacter = 'b';
+    int num = 10;
     printf("%c, %c", firstCharacter, secondCharacter);
+    printf("\n");
+    printf("%d", num);
+    printf("\n");
+    printf("%p", &num);
+
 
 }
