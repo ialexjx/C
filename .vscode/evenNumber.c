2d650d39d243622bae0641137e3521c6040e6262
ialexjx <akshathrx6393@gmail.com>
2023-11-27 23:40:23 +0530

    even numbers

---
+++ .vscode/evenNumber.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int num = 22;
+    if(num % 2 == 0){
+        printf("The number is even");
+    }else{
+        printf("Number is odd");
+    }
+    return 0;
+}
