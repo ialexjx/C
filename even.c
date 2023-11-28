0c9d0aa8f0b6d2a611305a0b16b0c0c72b65eb7f
ialexjx <akshathrx6393@gmail.com>
2023-11-28 23:11:28 +0530

    Even number program

---
+++ even.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main(){
+    int num;
+    printf("The number is %d", num);
+    if(num % 2 == 0){
+        printf("Number is even");
+    }else{
+        printf("THe number is odd");
+    }
+}
