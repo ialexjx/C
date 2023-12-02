13627fd036592d0b68be31e60ce5f93eb9e1df3f
ialexjx <akshathrx6393@gmail.com>
2023-12-02 23:21:44 +0530

    square

---
+++ square.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int side;
+    printf("Enter the side length: ");
+    scanf("%d", &side);
+
+    for (int i = 1; i <= side; i++) {
+        for (int j = 1; j <= side; j++)
+            printf("* ");
+        printf("\n");
+    }
+
+    return 0;
+}
