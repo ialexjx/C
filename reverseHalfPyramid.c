b2e1271250b5e1d12bfac0abcaac5462956ae6e3
ialexjx <akshathrx6393@gmail.com>
2023-12-06 22:47:57 +0530

    Reversed half pyramid

---
+++ reverseHalfPyramid.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+  int n;
+  printf("Enter number of rows: ");
+  scanf("%d", &n);
+
+  for (int i = n; i >= 1; i--) {
+    for (int j = 1; j <= i; j++) {
+      printf("*");
+    }
+    printf("\n");
+  }
+
+  return 0;
+}
