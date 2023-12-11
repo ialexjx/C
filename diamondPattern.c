8a5033a130f069ebccab67e0cdab0f9e46fce765
ialexjx <akshathrx6393@gmail.com>
2023-12-11 20:07:33 +0530

    no change

--- diamondPattern.c
+++ diamondPattern.c
@@ -1,23 +1,23 @@
-#include <stdio.h>
+// #include <stdio.h>
 
-int main() {
-  int n;
-  printf("Enter number of rows: ");
-  scanf("%d", &n);
+// int main() {
+//   int n;
+//   printf("Enter number of rows: ");
+//   scanf("%d", &n);
 
-  for (int i = 1; i <= n; i++) {
-    for (int j = 1; j <= i; j++) {
-      printf("*");
-    }
-    printf("\n");
-  }
+//   for (int i = 1; i <= n; i++) {
+//     for (int j = 1; j <= i; j++) {
+//       printf("*");
+//     }
+//     printf("\n");
+//   }
 
-  for (int i = n - 1; i >= 1; i--) {
-    for (int j = 1; j <= i; j++) {
-      printf("*");
-    }
-    printf("\n");
-  }
+//   for (int i = n - 1; i >= 1; i--) {
+//     for (int j = 1; j <= i; j++) {
+//       printf("*");
+//     }
+//     printf("\n");
+//   }
 
-  return 0;
-}
+//   return 0;
+// }
