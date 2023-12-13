8bffdadc8856fc4fd3968f7e8f94f1804f05fc1f
ialexjx <akshathrx6393@gmail.com>
2023-12-13 21:38:29 +0530

    No change

--- square.c
+++ square.c
@@ -1,15 +1,15 @@
-#include <stdio.h>
+// #include <stdio.h>
 
-int main() {
-    int side;
-    printf("Enter the side length: ");
-    scanf("%d", &side);
+// int main() {
+//     int side;
+//     printf("Enter the side length: ");
+//     scanf("%d", &side);
 
-    for (int i = 1; i <= side; i++) {
-        for (int j = 1; j <= side; j++)
-            printf("* ");
-        printf("\n");
-    }
+//     for (int i = 1; i <= side; i++) {
+//         for (int j = 1; j <= side; j++)
+//             printf("* ");
+//         printf("\n");
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
